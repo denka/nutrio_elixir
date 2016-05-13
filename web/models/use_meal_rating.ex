390391e5c364488a1dafef62bc7db0461d2907bc
Denisse C <denisse.cayetano@gmail.com>
2016-05-13 16:17:12 -0400

    value validation

--- web/models/use_meal_rating.ex
+++ web/models/use_meal_rating.ex
@@ -1,5 +1,6 @@
 defmodule NutrioElixir.UserMealRating do
   use Ecto.Model
+  import Ecto.Changeset
 
   @primary_key {:rating_id, :integer, []}
   @derive {Poison.Encoder, only: [:rating_id, :meal_id, :value]}
@@ -8,5 +9,11 @@ defmodule NutrioElixir.UserMealRating do
     field :value, :decimal
     field :cobrand_id, :integer
     belongs_to :user, NutrioElixir.User, foreign_key: :user_id
+  end
+  
+  def changeset(user_meal_rating, params \\ %{}) do
+    user_meal_rating
+    |> cast(params, ~w(meal_id value cobrand_id))
+    |> validate_number(:value, less_than: 101)
   end
 end
